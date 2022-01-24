c380f4569ec48c586e90a107ffa2ed75c3afe457
Lucas <lucasferreiraok@outlook.com>
2022-01-24 10:24:53 -0300

    index modificado

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(process.env.PORT);
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log("%s listening at " + PORT); // eslint-disable-line no-console
   });
 });
